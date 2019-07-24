243f6924a7b57e5cc8f069c5b1353000d0ab3cee
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-24 18:01:17 +0300

    Add comments

--- app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivity.kt
@@ -9,6 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // We can write Anko Layout DSL code right here,
+        // but in this example we define UI in AnkoComponent in a separate file.
+        // Here we use this component in our activity.
+
         MainActivityUI().setContentView(this)
     }
 }
--- app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivityUI.kt
+++ app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivityUI.kt
@@ -2,11 +2,14 @@ package com.gpetuhov.android.sampleankolayouts
 import org.jetbrains.anko.*
 import org.jetbrains.anko.sdk25.listeners.onClick
 
+// This is the code for MainActivity UI.
+// We have no XML layout at all.
 class MainActivityUI : AnkoComponent<MainActivity> {
     override fun createView(ui: AnkoContext<MainActivity>) = with(ui) {
         verticalLayout {
             val name = editText()
             button("Say Hello") {
+                // To use this we need to add Anko Listeners dependency in our build script
                 onClick { ctx.toast("Hello, ${name.text}!") }
             }
         }
