a09eb1cb4fb9c62b24b9f8f3e2f0ee4d274da2ef
Gennady Petukhov <gpetukhov@phoenixit.ru>
2019-07-24 17:45:02 +0300

    Add Anko Layouts

--- app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivity.kt
+++ app/src/main/java/com/gpetuhov/android/sampleankolayouts/MainActivity.kt
@@ -2,11 +2,26 @@ package com.gpetuhov.android.sampleankolayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.jetbrains.anko.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        verticalLayout {
+            padding = dip(30)
+            editText {
+                hint = "Name"
+                textSize = 24f
+            }
+            editText {
+                hint = "Password"
+                textSize = 24f
+            }
+            button("Login") {
+                textSize = 26f
+            }
+        }
     }
 }
